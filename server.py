cdb2b42caf704ecd9558683bb8a1fb3a20475098
“LunaWang” <wangzhiyue993@gmail.com>
2026-05-24 09:37:08 -0700

    Fix navigation links in case study page and add local server script

---
+++ server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import http.server
+import socketserver
+import os
+from pathlib import Path
+
+PORT = int(os.environ.get('PORT', 8000))
+os.chdir(Path(__file__).parent)
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Server running at http://localhost:{PORT}/")
+    httpd.serve_forever()
